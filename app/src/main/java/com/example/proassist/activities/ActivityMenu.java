ea66e143fac53909c8c7f744d64baa3638c97e5f
David Beltran Esperilla <117736090+davidbelesp@users.noreply.github.com>
2024-02-29 13:52:35 +0100

    Main Activities creation

--- app/src/main/java/com/example/proassist/activities/ActivityMenu.java
+++ app/src/main/java/com/example/proassist/activities/ActivityMenu.java
@@ -1,14 +1,16 @@
-package com.example.proassist;
+package com.example.proassist.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.proassist.R;
+
+public class ActivityMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_menu);
     }
 }
